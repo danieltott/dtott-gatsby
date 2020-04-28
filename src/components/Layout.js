c1bae73c9f2fd199eae0b0da9cb61a648c0b178e
Dan Ott <dan@dtott.com>
2020-04-28 15:05:01 -0400

    navigate not being used

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { StaticQuery, graphql, navigate } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 import moment from 'moment'
 import Header from './Header'
 import '../styles/codetheme.css'
